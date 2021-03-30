58035a3306d2ee4c84fb1a2faa8530f373307b1a
Bala <sbala@akamai.com>
2021-03-30 15:48:55 -0400

    error message

--- src/utils/http-error-message.ts
+++ src/utils/http-error-message.ts
@@ -9,6 +9,7 @@ export enum ErrorMessage {
     GET_CONTRACT_ERROR = "ERROR: Unable to retrieve contracts for your account.",
     GET_RESTIER_ERROR = "ERROR: Unable to retrieve resource tiers for the contract.",
     GET_RESTR_FOR_EW_ERROR = "ERROR: Unable to retrieve resource tiers for the specified Edgeworker id.",
+    GET_RESTR_FOR_EW_404 = "Unable to find the requested EdgeWorker id.",
     LISTALL_EW_404 = "The requested resource Tier is not found.",
     UPDATE_EW_400 = "Cloning an EdgeWorkers id is required in order to edit the Resource Tier id."
 }
