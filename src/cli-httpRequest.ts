590a4f3443100fa9e588703d4fb1024779f68b86
Swathi Bala <swathimr@users.noreply.github.com>
2022-02-17 15:47:41 -0500

    adding metric in the header (#64)
    Adding metrics in header for ekv

--- src/cli-httpRequest.ts
+++ src/cli-httpRequest.ts
@@ -2,9 +2,13 @@ import * as envUtils from './utils/env-utils';
 import * as cliUtils from './utils/cli-utils';
 import { promiseTimeout } from './utils/timeout-promise';
 import { EDGEWORKERS_API_BASE, EDGEWORKERS_CLIENT_HEADER } from './edgeworkers/ew-service';
+import { EDGEKV_API_BASE } from './edgekv/ekv-service';
 
 export var accountKey: string = null;
 export var timeoutVal: number = 120000;
+const versionHeader = "X-AK-EDGEKV-CLI-VER"
+const ekvcliHeader = "X-AK-EDGEKV-CLI"
+let pjson = require('../package.json');
 
 export function setAccountKey(account: string) {
     accountKey = account;
@@ -14,7 +18,7 @@ export function setAccountKey(account: string) {
  * This is for non-Tarball gets and all POST/PUT actions that return JSON or string bodies
  * for both edge CLI and edge KV CLI. This method authenticates, sends request and returns promise
  */
-export function sendEdgeRequest(pth: string, method: string, body, headers, timeout: number) {
+export function sendEdgeRequest(pth: string, method: string, body, headers, timeout: number, metricType?: string) {
     const edge = envUtils.getEdgeGrid();
     var path = pth;
     var qs: string = "&";
@@ -27,6 +31,12 @@ export function sendEdgeRequest(pth: string, method: string, body, headers, time
     if (path.includes(EDGEWORKERS_API_BASE)) {
       headers[EDGEWORKERS_CLIENT_HEADER] = "CLI";
     }
+
+  if (path.includes(EDGEKV_API_BASE)) {
+    headers[versionHeader] = pjson.version;
+    headers[ekvcliHeader] = metricType;
+    }
+
 
     let servicePromise = function () {
         return new Promise<any>(
@@ -68,24 +78,24 @@ export function sendEdgeRequest(pth: string, method: string, body, headers, time
     return promiseTimeout(timeout, servicePromise());
 }
 
-export function postJson(path: string, body, timeout: number) {
+export function postJson(path: string, body, timeout: number, metricType?: string) {
     return sendEdgeRequest(path, 'POST', body, {
         'Content-Type': 'application/json'
-    }, timeout);
+    }, timeout, metricType);
 }
 
-export function putJson(path: string, body, timeout: number) {
+export function putJson(path: string, body, timeout: number, metricType?: string) {
     return sendEdgeRequest(path, 'PUT', body, {
         'Content-Type': 'application/json'
-    }, timeout);
+    }, timeout, metricType);
 }
 
-export function getJson(path: string, timeout: number) {
-    return sendEdgeRequest(path, 'GET', '', {}, timeout);
+export function getJson(path: string, timeout: number, metricType?: string ) {
+    return sendEdgeRequest(path, 'GET', '', {}, timeout, metricType);
 }
 
-export function deleteReq(path: string, timeout: number) {
-    return sendEdgeRequest(path, 'DELETE', '', {}, timeout);
+export function deleteReq(path: string, timeout: number, metricType?: string) {
+    return sendEdgeRequest(path, 'DELETE', '', {}, timeout, metricType);
 }
 
 export function isOkStatus(code) {
