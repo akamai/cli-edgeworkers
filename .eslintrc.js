ae820f3c9c1bce482368376516577564038622d8
Kyle Li <kyli@akamai.com>
2022-07-14 16:57:37 -0400

    EW-12363 Fix broken error handling in cli-httpRequest
    - fixed error handling in cli-httpRequest
    - added unit tests for the error handing fix
    - updated typescript library version
    - added jest library to implement unit tests
    - added eslint library to check ts format

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "es2021": true,
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "semi": ["error", "always"],
+        "quotes": ["warn", "single"]
+    }
+}
